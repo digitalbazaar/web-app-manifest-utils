d7c1a790bf3b824095d93f2760e52c2b76e004dc
Josh Dunn <jdunn@digitalbazaar.com>
2020-06-29 09:54:51 -0600

    Update client name and add new client function.
    Also add fallback for favicon if manifest does not exist.

--- WebAppManifestClient.js
+++ WebAppManifestClient.js
@@ -2,32 +2,31 @@
  * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
  */
 import {httpClient} from '@digitalbazaar/http-client';
+import {getWebAppManifestIcon} from './manifest.js';
 
 const DEFAULT_HEADERS = {Accept: 'application/manifest+json, application/json'};
 
-export class ManifestClient {
+export class WebAppManifestClient {
   constructor({
     defaultHeaders,
-    host,
     httpsAgent,
     manifestProxyHost,
     manifestProxyPath = '/manifest',
     manifestProxy = false
   } = {}) {
     this.defaultHeaders = {...DEFAULT_HEADERS, ...defaultHeaders};
-    this.host = host;
     this.httpsAgent = httpsAgent;
     this.manifestProxyHost = manifestProxyHost;
     this.manifestProxyPath = manifestProxyPath;
     this.manifestProxy = manifestProxy;
   }
 
-  async getManifest() {
+  async getManifest({origin}) {
     const {
       host, defaultHeaders: headers, httpsAgent, manifestProxyHost,
       manifestProxyPath, manifestProxy
     } = this;
-    const url = `https://${host}/manifest.json`;
+    const url = `https://${origin}/manifest.json`;
     let result;
     try {
       result = await httpClient.get(url, {headers, httpsAgent});
@@ -48,4 +47,30 @@ export class ManifestClient {
     });
     return result.data;
   }
+
+  async getManifestWithIcon({origin, size}) {
+    const {defaultHeaders: headers, httpsAgent} = this;
+    let manifest = null;
+    let icon = {};
+    try {
+      manifest = await this.getManifest({origin});
+    } catch(err) {
+      const favicon = await getFavicon({origin, headers, httpsAgent});
+      if(favicon) {
+        icon.src = favicon;
+        return {manifest, icon};
+      }
+    }
+    icon = await getWebAppManifestIcon({manifest, origin, size});
+    return {manifest, icon};
+  }
+}
+
+async function getFavicon({origin, headers, httpsAgent}) {
+  const url = `https://${origin}/favicon.ico`;
+  const result = await httpClient.head(url, {headers, httpsAgent});
+  if(result.status === 200) {
+    return url;
+  }
+  return null;
 }
--- tests/10-ManifestClient.spec.js
+++ tests/10-ManifestClient.spec.js
@@ -1,27 +1,24 @@
 /*!
  * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
  */
-import {ManifestClient} from '..';
+import {WebAppManifestClient} from '..';
 import https from 'https';
 import isNode from 'detect-node';
 
 const httpsAgent = new https.Agent({rejectUnauthorized: false});
-const host = 'frontendmasters.com';
+let origin = 'frontendmasters.com';
 
 describe(`Manifest Client`, () => {
   describe(`'/manifest.json' Tests`, () => {
     if(isNode) {
-      it(`successful manifest response with icons'`,
+      it(`successful manifest response'`,
         async () => {
-          const manifestClient = new ManifestClient({
-            host,
-            httpsAgent,
-          });
+          const manifestClient = new WebAppManifestClient({httpsAgent});
 
           let result;
           let err;
           try {
-            result = await manifestClient.getManifest();
+            result = await manifestClient.getManifest({origin});
           } catch(e) {
             err = e;
           }
@@ -34,12 +31,101 @@ describe(`Manifest Client`, () => {
       // KARMA TEST Only
       it(`fails due to CORS error'`,
         async () => {
-          const manifestClient = new ManifestClient({host});
+          const manifestClient = new WebAppManifestClient();
 
           let result;
           let err;
           try {
-            result = await manifestClient.getManifest();
+            result = await manifestClient.getManifest({origin});
+          } catch(e) {
+            err = e;
+          }
+          console.log('ERROR --------->', err);
+          should.exist(err);
+          should.not.exist(result);
+        }
+      );
+    }
+  });
+
+  describe(`'/manifest.json' With Icon Tests`, () => {
+    if(isNode) {
+      it(`successful manifest response with icons'`,
+        async () => {
+          const manifestClient = new WebAppManifestClient({httpsAgent});
+          const size = 192;
+
+          let result;
+          let err;
+          try {
+            result = await manifestClient.getManifestWithIcon({size, origin});
+          } catch(e) {
+            err = e;
+          }
+          should.exist(result);
+          should.not.exist(err);
+          result.should.be.an('object');
+          result.should.include.keys(['manifest', 'icon']);
+          result.manifest.should.be.an('object');
+          result.icon.should.be.an('object');
+        }
+      );
+    } else {
+      // KARMA TEST Only
+      it(`fails due to CORS error'`,
+        async () => {
+          const manifestClient = new WebAppManifestClient();
+          const size = 192;
+
+          let result;
+          let err;
+          try {
+            result = await manifestClient.getManifestWithIcon({size, origin});
+          } catch(e) {
+            err = e;
+          }
+          console.log('ERROR --------->', err);
+          should.exist(err);
+          should.not.exist(result);
+        }
+      );
+    }
+  });
+
+  describe(`'/manifest.json' With Favicon Fallback Tests`, () => {
+    if(isNode) {
+      it(`successful manifest response with favicon'`,
+        async () => {
+          const manifestClient = new WebAppManifestClient({httpsAgent});
+          origin = 'w3.org';
+          const size = 192;
+
+          let result;
+          let err;
+          try {
+            result = await manifestClient.getManifestWithIcon({origin, size});
+          } catch(e) {
+            err = e;
+          }
+          should.exist(result);
+          should.not.exist(err);
+          result.should.be.an('object');
+          result.should.include.keys(['manifest', 'icon']);
+          should.equal(result.manifest, null);
+          result.icon.should.be.an('object');
+        }
+      );
+    } else {
+      // KARMA TEST Only
+      it(`fails due to CORS error'`,
+        async () => {
+          const manifestClient = new WebAppManifestClient();
+          const size = 192;
+
+          let result;
+          let err;
+          try {
+            result = await manifestClient.getManifestWithIcon({size, origin});
           } catch(e) {
             err = e;
           }
