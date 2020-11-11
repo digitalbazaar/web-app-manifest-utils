ed82679bbbbedcb576e3b285a6cf963f0d2c917b
Josh Dunn <jdunn@digitalbazaar.com>
2020-11-11 09:54:51 -0700

    Update getManifestWithIcon functionality.
    Also adds color_scheme check.
    Also adds nock server to tests.

--- manifest.js
+++ manifest.js
@@ -2,13 +2,16 @@
  * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
  */
 
-export function getWebAppManifestIcon({manifest, origin, size}) {
+export function getWebAppManifestIcon({colorScheme, manifest, origin, size}) {
   let best = null;
   // find largest square icon that is at least 48px wide
   if(manifest && manifest.icons) {
     for(const icon of manifest.icons) {
       try {
-        const {sizes, src} = icon;
+        const {color_scheme, sizes, src} = icon;
+        if(color_scheme && color_scheme !== colorScheme) {
+          continue;
+        }
         if(typeof sizes === 'string' && typeof src === 'string') {
           let [x, y] = sizes.split('x');
           x = parseInt(x, 10);
@@ -21,6 +24,10 @@ export function getWebAppManifestIcon({manifest, origin, size}) {
           if(x === size && y === size) {
             // ideal match found
             best = {x, y, src};
+            // adds color_scheme to 'best' if it exists.
+            if(color_scheme) {
+              best.color_scheme = color_scheme;
+            }
             break;
           }
           const delta = Math.abs(size - x);
@@ -30,6 +37,10 @@ export function getWebAppManifestIcon({manifest, origin, size}) {
           // 3. current icon is closer to `size` than best icon so far
           if(!best || (best.x < 48 && x >= 48) || delta < best.delta) {
             best = {x, y, src, delta};
+            // adds color_scheme to 'best' if it exists.
+            if(color_scheme) {
+              best.color_scheme = color_scheme;
+            }
           }
         }
       } catch(e) {}
--- tests/10-ManifestClient.spec.js
+++ tests/10-ManifestClient.spec.js
@@ -4,15 +4,20 @@
 import {WebAppManifestClient} from '..';
 import https from 'https';
 import isNode from 'detect-node';
+import {mockManifests} from './mock-data.js';
+import nock from 'nock';
 
 const httpsAgent = new https.Agent({rejectUnauthorized: false});
-let origin = 'frontendmasters.com';
+const origin = 'example.com';
 
-describe(`Manifest Client`, () => {
+describe(`Manifest Client Nock Tests`, () => {
   describe(`'/manifest.json' Tests`, () => {
     if(isNode) {
       it(`successful manifest response'`,
         async () => {
+          const manifestType = 'basic';
+          _nockManifest({manifestType});
+
           const manifestClient = new WebAppManifestClient({httpsAgent});
 
           let result;
@@ -48,10 +53,13 @@ describe(`Manifest Client`, () => {
     }
   });
 
-  describe(`'/manifest.json' With Icon Tests`, () => {
+  describe(`'/manifest.json' Icon Tests`, () => {
     if(isNode) {
-      it(`successful manifest response with icons'`,
+      it(`manifest response with icon'`,
         async () => {
+          const manifestType = 'basic';
+          _nockManifest({manifestType});
+
           const manifestClient = new WebAppManifestClient({httpsAgent});
           const size = 192;
 
@@ -68,6 +76,60 @@ describe(`Manifest Client`, () => {
           result.should.include.keys(['manifest', 'icon']);
           result.manifest.should.be.an('object');
           result.icon.should.be.an('object');
+        }
+      );
+      it(`manifest response with light icon'`,
+        async () => {
+          const manifestType = 'full';
+          _nockManifest({manifestType});
+
+          const manifestClient = new WebAppManifestClient({httpsAgent});
+          const size = 192;
+          const colorScheme = 'light';
+
+          let result;
+          let err;
+          try {
+            result = await manifestClient.getManifestWithIcon({
+              colorScheme, size, origin
+            });
+          } catch(e) {
+            err = e;
+          }
+          should.exist(result);
+          should.not.exist(err);
+          result.should.be.an('object');
+          result.should.include.keys(['manifest', 'icon']);
+          result.manifest.should.be.an('object');
+          result.icon.should.be.an('object');
+          result.icon.color_scheme.should.equal('light');
+        }
+      );
+      it(`manifest response with dark icon'`,
+        async () => {
+          const manifestType = 'full';
+          _nockManifest({manifestType});
+
+          const manifestClient = new WebAppManifestClient({httpsAgent});
+          const size = 192;
+          const colorScheme = 'dark';
+
+          let result;
+          let err;
+          try {
+            result = await manifestClient.getManifestWithIcon({
+              colorScheme, size, origin
+            });
+          } catch(e) {
+            err = e;
+          }
+          should.exist(result);
+          should.not.exist(err);
+          result.should.be.an('object');
+          result.should.include.keys(['manifest', 'icon']);
+          result.manifest.should.be.an('object');
+          result.icon.should.be.an('object');
+          result.icon.color_scheme.should.equal('dark');
         }
       );
     } else {
@@ -94,10 +156,13 @@ describe(`Manifest Client`, () => {
 
   describe(`'/manifest.json' With Favicon Fallback Tests`, () => {
     if(isNode) {
-      it(`successful manifest response with favicon'`,
+      it(`no manifest exists with favicon'`,
         async () => {
+          const manifestType = 'no-manifest';
+          _nockManifest({manifestType});
+          _nockFavicon({status: 200});
+
           const manifestClient = new WebAppManifestClient({httpsAgent});
-          origin = 'w3.org';
           const size = 192;
 
           let result;
@@ -111,8 +176,110 @@ describe(`Manifest Client`, () => {
           should.not.exist(err);
           result.should.be.an('object');
           result.should.include.keys(['manifest', 'icon']);
-          should.equal(result.manifest, null);
+          should.equal(result.manifest, undefined);
           result.icon.should.be.an('object');
+          result.icon.src.should.include('favicon.ico');
+        }
+      );
+      it(`manifest with no icons with favicon'`,
+        async () => {
+          const manifestType = 'no-icons';
+          _nockManifest({manifestType});
+          _nockFavicon({status: 200});
+
+          const manifestClient = new WebAppManifestClient({httpsAgent});
+          const size = 192;
+
+          let result;
+          let err;
+          try {
+            result = await manifestClient.getManifestWithIcon({
+              size, origin
+            });
+          } catch(e) {
+            err = e;
+          }
+          should.exist(result);
+          should.not.exist(err);
+          result.should.be.an('object');
+          result.should.include.keys(['manifest', 'icon']);
+          result.manifest.should.be.an('object');
+          result.icon.should.be.an('object');
+          result.icon.src.should.include('favicon.ico');
+        }
+      );
+    } else {
+      // KARMA TEST Only
+      it(`fails due to CORS error'`,
+        async () => {
+          const manifestClient = new WebAppManifestClient();
+          const size = 192;
+
+          let result;
+          let err;
+          try {
+            result = await manifestClient.getManifestWithIcon({size, origin});
+          } catch(e) {
+            err = e;
+          }
+          console.log('ERROR --------->', err);
+          should.exist(err);
+          should.not.exist(result);
+        }
+      );
+    }
+  });
+
+  describe(`'/manifest.json' With No Favicon Tests`, () => {
+    if(isNode) {
+      it(`no manifest exists with no favicon'`,
+        async () => {
+          const manifestType = 'no-manifest';
+          _nockManifest({manifestType});
+          _nockFavicon({status: 404});
+
+          const manifestClient = new WebAppManifestClient({httpsAgent});
+          const size = 192;
+
+          let result;
+          let err;
+          try {
+            result = await manifestClient.getManifestWithIcon({origin, size});
+          } catch(e) {
+            err = e;
+          }
+          should.exist(result);
+          should.not.exist(err);
+          result.should.be.an('object');
+          result.should.include.keys(['manifest', 'icon']);
+          should.equal(result.manifest, undefined);
+          should.equal(result.icon, undefined);
+        }
+      );
+      it(`manifest with no icons with no favicon'`,
+        async () => {
+          const manifestType = 'no-icons';
+          _nockManifest({manifestType});
+          _nockFavicon({status: 404});
+
+          const manifestClient = new WebAppManifestClient({httpsAgent});
+          const size = 192;
+
+          let result;
+          let err;
+          try {
+            result = await manifestClient.getManifestWithIcon({
+              size, origin
+            });
+          } catch(e) {
+            err = e;
+          }
+          should.exist(result);
+          should.not.exist(err);
+          result.should.be.an('object');
+          result.should.include.keys(['manifest', 'icon']);
+          result.manifest.should.be.an('object');
+          should.equal(result.icon, undefined);
         }
       );
     } else {
@@ -137,3 +304,35 @@ describe(`Manifest Client`, () => {
     }
   });
 });
+
+function _nockManifest({manifestType}) {
+  nock(`https://${origin}`)
+    .get('/manifest.json')
+    // eslint-disable-next-line no-unused-vars
+    .reply(uri => {
+      let manifest;
+      switch(manifestType) {
+        case 'basic':
+          manifest = mockManifests.basicManifest;
+          break;
+        case 'full':
+          manifest = mockManifests.fullManifest;
+          break;
+        case 'no-icons':
+          manifest = mockManifests.noIconsManifest;
+          break;
+        case 'no-manifest':
+          return [404];
+      }
+      return [200, manifest];
+    });
+}
+
+function _nockFavicon({status}) {
+  nock(`https://${origin}`)
+    .head('/favicon.ico')
+    // eslint-disable-next-line no-unused-vars
+    .reply(uri => {
+      return [status];
+    });
+}
