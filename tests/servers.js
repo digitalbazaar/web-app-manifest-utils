dbf4cd686cd7afc49c8020383509049cf91a82a9
Josh Dunn <jdunn@digitalbazaar.com>
2020-11-16 17:05:58 -0700

    Update server and remove nock.

--- tests/servers.js
+++ tests/servers.js
@@ -3,24 +3,66 @@
  */
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
+const https = require('https');
+const {mockManifests} = require('./mock-data.js');
 
 const server1 = express();
 const server2 = express();
 
-// server without CORS
-server1.get('/manifest.json', function(req, res) {
-  res.json({msg: 'Web server without CORS'});
+// web server without CORS
+server1.get('/basic/manifest.json', function(req, res) {
+  const manifest = mockManifests.basicManifest;
+  res.json(manifest);
 });
-server1.listen(19450, function() {
+server1.get('/full/manifest.json', function(req, res) {
+  const manifest = mockManifests.fullManifest;
+  res.json(manifest);
+});
+server2.get('/no-icons/manifest.json', function(req, res) {
+  const manifest = mockManifests.noIconsManifest;
+  res.json(manifest);
+});
+server1.get('/no-manifest/manifest.json', function(req, res) {
+  const manifest = undefined;
+  res.json(manifest);
+});
+// server1.listen(19450, function() {
+//   console.log('Web server without CORS listening on port 19450');
+// });
+https.createServer({
+  key: fs.readFileSync(__dirname + '/key.pem'),
+  cert: fs.readFileSync(__dirname + '/cert.pem')
+}, server1).listen(19450, function() {
   console.log('Web server without CORS listening on port 19450');
 });
 
-// server with CORS
+// web server with CORS
 server2.use(cors());
-server2.get('/manifest.json', function(req, res) {
-  res.json({msg: 'This is CORS-enabled for all origins!'});
+server2.get('/basic/manifest.json', function(req, res) {
+  const manifest = mockManifests.basicManifest;
+  res.json(manifest);
 });
-server2.listen(19451, function() {
+server2.get('/full/manifest.json', function(req, res) {
+  const manifest = mockManifests.fullManifest;
+  res.json(manifest);
+});
+server2.get('/no-icons/manifest.json', function(req, res) {
+  const manifest = mockManifests.noIconsManifest;
+  res.json(manifest);
+});
+server2.get('/no-manifest/manifest.json', function(req, res) {
+  const manifest = undefined;
+  res.json(manifest);
+});
+
+// server2.listen(19451, function() {
+//   console.log('CORS-enabled web server listening on port 19451');
+// });
+https.createServer({
+  key: fs.readFileSync(__dirname + '/key.pem'),
+  cert: fs.readFileSync(__dirname + '/cert.pem')
+}, server2).listen(19451, function() {
   console.log('CORS-enabled web server listening on port 19451');
 });
 
